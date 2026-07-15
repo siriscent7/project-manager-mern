6a2c81775ddd3d8c48bb22f2cea3a6bebdc95a91
Anushka S Raghunandan <siriscent7@gmail.com>
2026-07-15 10:48:23 +0530

    Update api.js

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:5001/api'
+  baseURL: 'https://project-manager-backend-97b0.onrender.com'
 });
 
 // Add token to all requests
